21a32d3c9a821c068dea88806d92d440ecb149f6
jannucs <vamsi.cs2@gmail.com>
2019-10-23 00:38:51 +0530

    First commit - with 1. variables and datatypes

---
+++ 1. Variables and datatypes.py
@@ -0,0 +1,39 @@
+a_value=15
+a_result=(25-7)*17
+print(a_value)
+print(a_result+12)
+print(a_result+a_value)
+
+variable_1=20
+variable_2=20
+variable_2+=10
+variable_1*=4
+print(variable_1,variable_2)
+
+variable_1=10
+variable_2=2.5
+variable_1+=6.5
+variable_2*=2
+print(variable_1,variable_2)
+
+variable_a=13.9
+variable_b=2.8
+variable_a=round(variable_a)
+variable_b=int(variable_b)
+print(variable_a,variable_b)
+
+app_name="Pandora - Music & Radio"
+average_rating=4.0
+total_ratings=1724546
+price="free"
+print(app_name)
+
+motto='Facebook\'s new motto is "move fast with stable infra."'
+print(motto)
+
+facebook="Facebook's rating is "
+fb_rating=3.5
+fb_rating_str=str(fb_rating)
+fb=facebook+fb_rating_str
+print(fb)
+
