69a0400b4c98a5fbd638823de5ddeeef443c2f8c
jannucs <vamsi.cs2@gmail.com>
2019-10-25 19:49:08 +0530

    Execercise - 3 If statements conditional ops

---
+++ Exercise 3 - Conditionals if .py
@@ -0,0 +1,33 @@
+#Using an if statement to control your code:
+
+if True:
+    print(1)
+if 1 == 1:
+    print(2)
+    print(3)
+
+#Combining multiple conditions:
+
+if 3 > 1 and 'data == data':
+    print('Both conditions are true!')  
+if 10 < 20 or 4 <= 5:
+    print('At least one condition is true.')
+
+#Building more complex if statements:
+
+if (20 > 3 and 2 != 1) or 'Games' == 'Games':
+    print('At least one condition is true.')
+
+#Using the else clause:
+
+if False:
+    print(1)
+else:
+    print('The condition above was false.')
+
+#Using the elif clause:
+
+if False:
+    print(1)
+elif 30 > 5:
+    print('The condition above was false.')
