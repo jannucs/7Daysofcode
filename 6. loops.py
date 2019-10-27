0f6d97a275031a93f3a4c38b89484edb36fc03cf
bh13621 <jannu.vamsi.krishna@macys.com>
2019-10-27 23:43:45 +0530

    Adding Exceptions and loops

---
+++ 6. loops.py
@@ -0,0 +1,7 @@
+student_names = ["Jannu", "Krishna", "Vamsi", "Jannu", "Hey"]
+
+for name in student_names:
+	if name == "Jannu":
+		continue
+		print("Found him! " + name)
+	print("Currently testing " + name)
